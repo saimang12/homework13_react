50634a015d50a1fb03902c2a58645a2dec607202
saimang12@naver.com <saimang12@naver.com>
2023-03-03 17:50:22 +0900

    12321

--- src/components/MainProduct.js
+++ src/components/MainProduct.js
@@ -1,4 +1,7 @@
-const MainProduct = () => {
+import { useState } from "react";
+
+const MainProduct = ({ MAIN_CONTENT, slide }) => {
+    const [num, setNum] = useState(0)
     return (
         <section className="MainProduct">
             <div className="inner">
@@ -8,9 +11,17 @@ const MainProduct = () => {
                 </div>
                 <div className="tab_menu">
                     <ul className="tab_select">
-
+                        {
+                            slide.map((it, idx) => {
+                                return (
+                                    <li>{it.title}</li>
+                                )
+                            })
+                        }
                     </ul>
-
+                    <div className="tab_itm">
+                        213
+                    </div>
                 </div>
             </div>
         </section>
